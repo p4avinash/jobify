21812d1cb8ceb774902ca8b1b3b8e80d5be1c2bb
p4avinash <p4avinashkumar@gmail.com>
2024-03-09 13:31:06 +0530

    implemented theme toggle functionality between dark, light and system, implemented navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,20 @@
 import React from "react"
+import LinksDropdown from "./LinksDropdown"
+import { UserButton } from "@clerk/nextjs"
+import ThemeToggle from "./ThemeToggle"
 
 const Navbar = () => {
-  return <h2 className='text-4xl'>Navbar</h2>
+  return (
+    <nav className='bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between'>
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className='flex items-center gap-x-4'>
+        <ThemeToggle />
+        <UserButton afterSignOutUrl={"/"} />
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
